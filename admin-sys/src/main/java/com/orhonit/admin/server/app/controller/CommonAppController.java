2c26582b1f6eeaa9ea0e372ee02767c8b0ef2eee
sunshaohui <ai13644881828@163.com>
2018-06-06 17:33:06 +0800

    根据通总的意思修改

--- admin-sys/src/main/java/com/orhonit/admin/server/app/controller/CommonAppController.java
+++ admin-sys/src/main/java/com/orhonit/admin/server/app/controller/CommonAppController.java
@@ -95,7 +95,7 @@ public class CommonAppController {
 	@Autowired
     private StudyVideoService studyVideoService;
 	@GetMapping("/studyvideo/frist")
-	@ApiOperation("观看最多的1条视频学习")
+	@ApiOperation("观看最多的2条视频学习")
 	public StudyVideo frist(){
 		return studyVideoService.frist();
 	}
@@ -107,7 +107,7 @@ public class CommonAppController {
 	@Autowired
 	private StudyvideomService studyvideomService;
 	@GetMapping("/studyvideom/frist")
-	@ApiOperation("蒙语观看最多的1条视频学习")
+	@ApiOperation("蒙语观看最多的2条视频学习")
 	public Studyvideom fristm(){
 		return studyvideomService.frist();
 	}
--- admin-sys/src/main/java/com/orhonit/admin/server/sys/dao/StudyVideoDao.java
+++ admin-sys/src/main/java/com/orhonit/admin/server/sys/dao/StudyVideoDao.java
@@ -44,7 +44,7 @@ public interface StudyVideoDao {
    
     @Select("select * from studyVideo where status=1 order by id desc limit #{start},10")
 	List<StudyVideo> ten(Long start);
-    @Select("select s.* from studyVideo s,category c where c.parentId = 11 and s.categoryId = c.id  and s.status = 1 order by s.clicks desc limit 0,1")
+    @Select("select s.* from studyVideo s,category c where c.parentId = 11 and s.categoryId = c.id  and s.status = 1 order by s.clicks desc limit 0,2")
 	StudyVideo frist();
     @Select("select * from studyVideo where status = 1")
 	List<StudyVideo> getAll();
